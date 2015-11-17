48a11b8a8a97f3abfa60b1bc783c56f9d85a33c7
Travis Clark <hiddenkrypt@gmail.com>
2015-11-17 11:43:01 -0500

    improved eatting and collisions, implemented basic camera and backround graphics to show motion

--- Nom/code.js
+++ Nom/code.js
@@ -32,19 +32,13 @@ var code = (function newDrabbleCode() {
 
 var engine = engine || (function () {
 		var settings = {
-			MAP_WIDTH : 600,
-			MAP_HEIGHT : 400
+			MAP_WIDTH : 1200,
+			MAP_HEIGHT : 800
 		};
 		var bits = [];
 		var keyStates = [];
 		var player = {};
-		var camera = {
-			x: 0,
-			y: 0,
-			w: code.settings.canvas.width,
-			h: code.settings.canvas.height,
-			bounds: .2
-		}
+		var camera = {};
 		var e = {
 			init : function () {
 
@@ -54,10 +48,12 @@ var engine = engine || (function () {
 				window.addEventListener("keyup", function (e) {
 					keyStates[e.keyCode] = false;
 				});
+				makeCamera();
 				makePlayer();
-				for (var i = 0; i < 28; i++) {
+				for (var i = 0; i < 255; i++) {
 					addBit();
 				}
+				//setInterval(reset, 3000 );
 			},
 			postInit : function () {},
 			test : function () {},
@@ -65,11 +61,30 @@ var engine = engine || (function () {
 				render();
 			}
 		};
-
+		function reset(){
+			bits = [];
+			player = {};
+			makePlayer();
+			makeCamera();
+			for (var i = 0; i < 255; i++) {
+				addBit();
+			}
+		}
 		function render() {
 			var ctx = drabble.ctx;
 			ctx.fillStyle = "#f0f0ff";
 			ctx.fillRect(0, 0, code.settings.canvas.WIDTH, code.settings.canvas.HEIGHT);
+			ctx.fillStyle = "#CACAFF";
+			for(var i = 0;  i < 255; i++){
+				ctx.fillRect((settings.MAP_WIDTH*2/i) - camera.x, 0, 1, code.settings.canvas.HEIGHT);
+				ctx.fillRect(0, (settings.MAP_HEIGHT*2/i) - camera.y, code.settings.canvas.WIDTH, 1);
+			}
+			ctx.fillStyle = "#9797FF";
+				ctx.fillRect(settings.MAP_WIDTH - camera.x, 0 - camera.y, 2, settings.MAP_HEIGHT);
+				ctx.fillRect(0 - camera.x, 					0 - camera.y, 2, settings.MAP_HEIGHT);
+				ctx.fillRect(0 - camera.x, 					0 - camera.y, settings.MAP_WIDTH, 2);
+				ctx.fillRect(0 - camera.x, settings.MAP_HEIGHT- camera.y, settings.MAP_WIDTH, 2);
+			
 			bits.forEach(function (bit) {
 				if (bit.size === 0) {
 					//bits.remove(bit);
@@ -78,6 +93,7 @@ var engine = engine || (function () {
 				bit.draw(ctx);
 			});
 			player.tock();
+			camera.tick();
 			player.draw(ctx);
 			requestAnimationFrame(render);
 		}
@@ -110,13 +126,40 @@ var engine = engine || (function () {
 			}
 			player.draw = function( ctx ){
 				ctx.fillStyle = "#ff5310";
-				ctx.strokeStyle = "#ffff00";	
+				ctx.strokeStyle = "#ffff00";
+					// ctx.fillStyle = "#696969";
+					// ctx.strokeStyle = "#000000";		
 				ctx.beginPath();
-				ctx.arc(player.x, player.y, player.size, 0, 2*Math.PI, false);
+				ctx.arc(player.x-camera.x, player.y-camera.y, player.size, 0, 2*Math.PI, false);
 				ctx.stroke();
 				ctx.fill();				
+				ctx.fillStyle= "#000000";
+				ctx.fillRect(this.x-camera.x, this.y-camera.y, 2, 2);
 			}
 			
+		}
+		function makeCamera(){
+			camera = {
+				x: 0,
+				y: 0,
+				w: code.settings.canvas.WIDTH,
+				h: code.settings.canvas.HEIGHT,
+				bounds: .2,
+				tick: function(){
+					while( player.x+player.size > this.x+this.w*0.8 ){
+						this.x++;
+					}
+					while( player.x < this.x*1.2 ){
+						this.x--;
+					}
+					while( player.y+player.size > this.y+this.h*0.8 ){
+						this.y++;
+					}
+					while( player.y < this.y*1.2 ){
+						this.y--;
+					}
+				}
+			};
 		}
 		function addBit() {
 			bits.push(new Bit());
@@ -150,32 +193,37 @@ var engine = engine || (function () {
 					ctx.fillStyle = "#696969";
 					ctx.strokeStyle = "#000000";	
 					ctx.beginPath();
-					ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI, false);
+					ctx.arc(this.x-camera.x, this.y-camera.y, this.size, 0, 2*Math.PI, false);
 					ctx.stroke();
-					ctx.fill();			
+					ctx.fill();		
+
+					ctx.fillStyle= "#000000";
+					ctx.fillRect(this.x-camera.x, this.y-camera.y, 2, 2);					
+					
+					
 				}
 			};
 		}
 		function collision(bitA, bitB) {
 			return (
 				Math.sqrt(
-					(bitA.x + bitA.size/2 - bitB.x - bitB.size/2) * (bitA.x + bitA.size/2 - bitB.x - bitB.size/2)
-					 + (bitA.y + bitA.size/2  - bitB.y - bitB.size/2 ) * (bitA.y + bitA.size/2  - bitB.y - bitB.size/2 )
-				) <= bitA.size + bitB.size
+					(bitA.x - bitB.x) * (bitA.x - bitB.x)
+					 + (bitA.y - bitB.y) * (bitA.y - bitB.y)
+				) <= (bitA.size + bitB.size)*0.9
 			);
 		}
 		function eat(bitA, bitB) {
 			if (collision(bitA, bitB)) {
 				if (bitA.size == bitB.size) {}
 				else if (bitA.size > bitB.size) {
-					bitA.size += Math.ceil(bitB.size / 2);
-					bitA.dx = bitA.dx * .9;
-					bitA.dy = bitA.dy * .9;
+					bitA.size += Math.ceil(bitB.size / Math.PI);
+					bitA.dx = bitA.dx * 1;
+					bitA.dy = bitA.dy * 1;
 					bitB.size = 0;
 				} else if (bitA.size < bitB.size) {
-					bitB.size += Math.ceil(bitA.size / 2);
-					bitB.dx = bitB.dx * .9;
-					bitB.dy = bitB.dy * .9;
+					bitB.size += Math.ceil(bitA.size / Math.PI);
+					bitB.dx = bitB.dx * 1;
+					bitB.dy = bitB.dy * 1;
 					bitA.size = 0;
 				}
 			}
