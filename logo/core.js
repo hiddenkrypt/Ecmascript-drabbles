888693df8ce55a115a4ca26bc1842435083f9d23
Travis Clark <hiddenkrypt@gmail.com>
2015-12-15 14:45:09 -0500

    Added OO namespacing

--- logo/core.js
+++ logo/core.js
@@ -1,79 +1,86 @@
-var color = {
-		bg: "#ffffff",
-		fg: "#000000"
-	},
-	c = {}, 
-	ctx = {},
-	cellsize = 1,
-	speed = {
-		fade: 1000,
-		punch: 75
+var core = (function(){
+	var color =  {
+			bg: "#ffffff",
+			fg: "#000000"
+		},
+		c = {}, 
+		ctx = {},
+		cellsize = 1,
+		speed = {
+			fade: 1000,
+			punch: 75
+		};
+
+	var coords = [
+		//r
+		{x:0, y:0},
+		{x:0, y:1},
+		{x:0, y:2},
+		{x:1, y:0},
+		{x:1, y:1},
+		{x:2, y:1},
+		{x:2, y:2},
+		//a
+		{x:3, y:1},
+		{x:3, y:2},
+		{x:4, y:0},
+		{x:4, y:2},
+		{x:5, y:1},
+		{x:5, y:2},
+		//v
+		{x:6, y:0},
+		{x:6, y:1},
+		{x:7, y:2},
+		{x:8, y:0},
+		{x:8, y:1},
+		//t
+		{x:9, y:0},
+		{x:10, y:0},
+		{x:10, y:1},
+		{x:10, y:2},
+		{x:11, y:0}
+	];
+
+	function init(){
+		c = document.getElementById("c");
+		cellsize = c.width/12;
+		ctx = c.getContext("2d");
+		coords = scramble(coords);
+		paintArray();
+	}
+
+	function paintArray(){
+		var i = 0;
+		function f(){
+			ctx.fillStyle = color.fg;
+			ctx.fillRect(coords[i].x*cellsize, coords[i].y*cellsize, cellsize, cellsize);
+			if(++i < coords.length){
+				setTimeout(f, speed.punch);
+			}			
+		}
+		ctx.fillStyle = color.bg;
+		ctx.fillRect(0,0,c.width, c.height);
+		setTimeout(f, speed.punch);
+	}
+	function sortLeftToRight(a, b){
+		return a.x < b.x;
+	}
+	function sortTopToBottom(a, b){
+		return a.y < b.y;
+	}
+
+	function scramble(array){
+		var temp = [];
+		while( array.length > 0){
+			var index = Math.floor(Math.random()*array.length);
+			temp = temp.concat( array.splice(index , 1) );
+		}
+		return temp;
+	}
+	return {
+		color: color,
+		speed: speed,
+		paintArray: paintArray,
+		init: init
 	};
-
-var coords = [
-	//r
-	{x:0, y:0},
-	{x:0, y:1},
-	{x:0, y:2},
-	{x:1, y:0},
-	{x:1, y:1},
-	{x:2, y:1},
-	{x:2, y:2},
-	//a
-	{x:3, y:1},
-	{x:3, y:2},
-	{x:4, y:0},
-	{x:4, y:2},
-	{x:5, y:1},
-	{x:5, y:2},
-	//v
-	{x:6, y:0},
-	{x:6, y:1},
-	{x:7, y:2},
-	{x:8, y:0},
-	{x:8, y:1},
-	//t
-	{x:9, y:0},
-	{x:10, y:0},
-	{x:10, y:1},
-	{x:10, y:2},
-	{x:11, y:0}
-];
-
-function init(){
-	initUI();
-	c = document.getElementById("c");
-	cellsize = c.width/12;
-	ctx = c.getContext("2d");
-	coords = scramble(coords);
-	paintArray();
-}
-
-function paintArray(){
-	var i = 0;
-	function f(){
-		ctx.fillStyle = color.fg;
-		ctx.fillRect(coords[i].x*cellsize, coords[i].y*cellsize, cellsize, cellsize);
-		if(++i < coords.length){
-			setTimeout(f, speed.punch);
-		}			
-	}
-	ctx.fillStyle = color.bg;
-	ctx.fillRect(0,0,c.width, c.height);
-	setTimeout(f, speed.punch);
-}
-function sortLeftToRight(a, b){
-	return a.x < b.x;
-}
-function sortTopToBottom(a, b){
-	return a.y < b.y;
-}
-
-function scramble(array){
-	var temp = [];
-	while( array.length > 0){
-		var index = Math.floor(Math.random()*array.length);
-		temp = temp.concat( array.splice(index , 1) );
-	}
-	return temp;
-}
+}());
--- logo/ui.js
+++ logo/ui.js
@@ -1,35 +1,38 @@
-var doc = {};
+var ui = (function(){
+	var doc = {};
+	
+	return{
+		initUI: function(){
+			doc = {
+				bg: document.getElementById("bgColor"),
+				fg: document.getElementById("fgColor"),
+				punchRate: document.getElementById("punchRate"),
+				fadeRate: document.getElementById("fadeRate")
+			};
+			doc.punchRate.value = core.speed.punch;
+		},
 
-function initUI(){
-	doc = {
-		bg: document.getElementById("bgColor"),
-		fg: document.getElementById("fgColor"),
-		punchRate: document.getElementById("punchRate"),
-		fadeRate: document.getElementById("fadeRate")
+		updateColor: function(){
+			core.color.bg = doc.bg.value;
+			core.color.fg = doc.fg.value;
+			core.paintArray();
+		},
+		updatePunchRate: function(){
+			core.speed.punch = doc.punchRate.value
+		},
+		updateFadeRate: function(){
+			core.speed.fade = doc.fadeRate.value
+		},
+
+		colorSwap: function(){
+			var tempColor = doc.bg.value;
+			doc.bg.value = doc.fg.value;
+			doc.fg.value = tempColor;
+			ui.updateColor();
+		},
+
+		run: function(){
+			core.paintArray();
+		}
 	};
-	doc.punchRate.value = speed.punch;
-}
-
-function updateColor(){
-	color.bg = doc.bg.value;
-	color.fg = doc.fg.value;
-	paintArray();
-}
-
-function updatePunchRate(){
-	speed.punch = doc.punchRate.value
-}
-function updateFadeRate(){
-	speed.fade = doc.fadeRate.value
-}
-
-function colorSwap(){
-	var tempColor = doc.bg.value;
-	doc.bg.value = doc.fg.value;
-	doc.fg.value = tempColor;
-	updateColor();
-}
-
-function run(){
-	paintArray();
-}
+}())
