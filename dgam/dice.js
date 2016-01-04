281f865bfe40831c40e1421b09add6ed008b409f
Travis Clark <hiddenkrypt@gmail.com>
2016-01-04 13:36:26 -0500

    Made Dice invalid string exception more explicit

--- dgam/dice.js
+++ dgam/dice.js
@@ -15,7 +15,7 @@ var Dice = (function(){
 	return { 
 		roll: function( diceString ){
 			if( !diceString.match(/^-?[0-9]+(d[0-9]+?)?(\+-?[0-9]+(d[0-9]+)?)*$/) ){
-				throw "Invalid Dice String";
+				throw "Invalid Dice String: "+diceString;
 			}
 			return diceString.split("+").map( e => rollElement(e) ).reduce( (b,a) => a+b );
 		},
