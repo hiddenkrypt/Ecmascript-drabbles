5700ddacc9961ddb74fb7939889664b68e3c8ca7
Travis Clark <hiddenkrypt@gmail.com>
2016-01-08 14:04:29 -0500

    Refactored public fields to use my publicAPI pattern. refactored cellular automata growth function to use coords objects

--- dgam/utils.js
+++ dgam/utils.js
@@ -1,6 +1,10 @@
 "use strict";
 var utils = (function(){
+	var publicAPI = {};
+	
 	var d = new Dice();
+	publicAPI.d = d;
+	
 	function buildMap(map){
 		map.land = [];
 		for(let i=0; i <= map.width; i++){
@@ -10,32 +14,29 @@ var utils = (function(){
 			}
 			map.land.push( row );
 		}
-	}
+	} publicAPI.buildMap = buildMap; 
+	
 	function territoryGenerationStep( territory, map ){
-		territory.lands.forEach( e => {
-			if( map.isLandFree( e.x-1, e.y ) && utils.d.roll("1d2") == 2 ){ // left
-				territory.lands.push( {x: e.x-1, y: e.y } );
-			}
-			if( map.isLandFree( e.x+1, e.y ) && utils.d.roll("1d2") == 2 ){ // right
-				territory.lands.push( {x: e.x+1, y: e.y } );
-			}
-			if( map.isLandFree( e.x, e.y-1 ) && utils.d.roll("1d2") == 2 ){ // top
-				territory.lands.push( {x: e.x, y: e.y-1 } );
-			}
-			if( map.isLandFree( e.x, e.y+1 ) && utils.d.roll("1d2") == 2 ){ // botm
-				territory.lands.push( {x: e.x, y: e.y+1 } );
-			}
+		territory.lands.forEach( land => {
+			var adjacentLands = [
+				{x:land.x+1, y:land.y+0},
+				{x:land.x-1, y:land.y-0},
+				{x:land.x+0, y:land.y+1},
+				{x:land.x-0, y:land.y-1}
+			];
+			
+			adjacentLands.forEach(neighbor => {
+				if( map.isLandFree( neighbor ) && utils.d.roll("1d2") == 2 ){ // left
+					territory.lands.push( neighbor );
+				}				
+			});
 		});
 		map.registerLands(territory);
-	}
+	} publicAPI.territoryGenerationStep = territoryGenerationStep;
+	
 	function containedInBox(x, y, left, right, top, bottom){
 		return x > left && x < right && y > top && y < bottom 
-	} 
+	} publicAPI.containedInBox = containedInBox;
 	
-	return {
-		d:d
-		,buildMap:buildMap
-		,territoryGenerationStep:territoryGenerationStep
-		,containedInBox:containedInBox
-	};
+	return publicAPI;
 }());
