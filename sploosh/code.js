e6f80a153284f17b132f2389eb5a1a301b269142
Rav-T <hiddenkrypt@gmail.com>
2018-01-07 14:30:54 -0800

    cleanup and refactoring

--- sploosh/code.js
+++ sploosh/code.js
@@ -1,98 +1,88 @@
+(function() {
+  var requestAnimationFrame =
+    window.requestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame
+    window.requestAnimationFrame = requestAnimationFrame
+})()
+
 var WIDTH = 300,
-	HEIGHT = 200,
-	canvas = {},
-	ctx = {};
+  HEIGHT = 200,
+  GRAVITY = .98,
+  WIND = .05,
+  BOUNCE = .6,
+  AIR_FRICTION = .99,
+  LOGIC_UPDATE_RATE = 30,
+  canvas = {},
+  ctx = {},
+  particleSize = 3,
+  particleCount = 200,
+  spawnRateLimit = 2,
+  bounceLimit = 4,
+  sourceX = 2,
+  sourceY = 50,
+  impulseX = 10,
+  impulseY = 10,
+  formX = -1.5,
+  formY = -.5,
+  ctx,
+  particles=[]
 
-var GRAVITY = .98
-	BOUNCE = .6,
-	AIRFRICTION = .99,
-	bitSize = 3,
-	bitCount = 200,
-	spawnRateLimit = 2,
-	bounceLimit = 4,
-	sourceX = 2,
-	sourceY = 50,
-	impulseX = 10,
-	impulseY = 10,
-	formX = -1.5,
-	formY = -.5,
-	ctx,
-	bits=[];
-
-	
-
-	
 function init(){
-	canvas = document.getElementById( 'c' );
-	ctx = canvas.getContext( "2d" );
-	canvas.style.width = canvas.width = WIDTH;
-	canvas.style.height = canvas.height = HEIGHT;
-	// window.addEventListener( "keydown", handleKeydown );
-	// window.addEventListener( "keyup", handleKeyup );
-	// canvas.addEventListener( "mousedown", handleMousedown );
-	setInterval( logicUpdate, 1000/30 );
-	renderUpdate();
+  canvas = document.getElementById( 'c' )
+  ctx = canvas.getContext( "2d" )
+  canvas.style.width = canvas.width = WIDTH
+  canvas.style.height = canvas.height = HEIGHT
+  setInterval( logicUpdate, 1000/LOGIC_UPDATE_RATE )
+  renderUpdate()
 }
+window.onload = init
 
 function renderUpdate(){
-	ctx.fillStyle = "black";
-	ctx.fillRect( 0, 0, WIDTH, HEIGHT );
-	bits.forEach(function(bit){
-		if( bit.bounced === 1){
-			ctx.fillStyle = "#3333ff";
-		} else if( bit.bounced > 1){
-			ctx.fillStyle = "#0000ff";
-		} else{
-			ctx.fillStyle = "#aabbff";
-		}
-		ctx.fillRect(bit.x, bit.y, bitSize, bitSize);
-	});
-	requestAnimationFrame( renderUpdate );
+  ctx.fillStyle = "black"
+  ctx.fillRect( 0, 0, WIDTH, HEIGHT )
+  particles.forEach(function(particle){
+    if( particle.bounced === 1){
+      ctx.fillStyle = "#3333ff"
+    } else if( particle.bounced > 1){
+      ctx.fillStyle = "#0000ff"
+    } else{
+      ctx.fillStyle = "#aabbff"
+    }
+    ctx.fillRect(particle.x, particle.y, particleSize, particleSize)
+  })
+  requestAnimationFrame( renderUpdate )
 }
 
-function getNewBit(){
-	return {
-		x: sourceX,
-		y: sourceY,
-		dx: (Math.random()+formX)*impulseX,
-		dy: (Math.random()+formY)*impulseY,
-		bounced: 0
-	}
+function Particle(){
+  this.x = sourceX
+  this.y = sourceY
+  this.dx = (Math.random()+formX)*impulseX
+  this.dy = (Math.random()+formY)*impulseY
+  this.bounced = 0
 }
+
 function logicUpdate(){
-	debugUpdate();
-	for(var i=0; i<bits.length; i++){
-		//motion
-		bits[i].x += bits[i].dx;
-		bits[i].y += bits[i].dy;
-		bits[i].dy+= GRAVITY;
-		bits[i].dx *= AIRFRICTION;
-		
-		if( bits[i].y >= HEIGHT && bits[i].bounced < bounceLimit ){ // floor bounce
-			bits[i].y = HEIGHT-5;
-			bits[i].dy = -bits[i].dy * BOUNCE;
-			bits[i].bounced++;
-		}
-		if( bits[i].x >= WIDTH - bitSize || bits[i].x <= 0 ){ // wall bounce
-			bits[i].dx = -bits[i].dx;
-		}
-		if( bits[i].y > HEIGHT && bits[i].bounced >= bounceLimit ){ //  bounce limit culling
-			bits.splice(i, 1);
-			console.log("cull "+bits.length);
-		}
-	}
-	for( var i = 0; i < spawnRateLimit && bits.length < bitCount; i++ ){
-		bits.push( getNewBit() );
-	}
+  particles.forEach(move)
+  for( var i = 0; i < spawnRateLimit && particles.length < particleCount; i++ ){ //spawn new particles up to maximum particleCount
+    particles.push( new Particle() )
+  }
+  particles = particles.filter(e=>{return !(e.y >= HEIGHT && e.bounced >= bounceLimit)}) //bounce limit culling
 }
 
-// function handleKeyup( event ){}
-// function handleKeydown( event ){}
-// function handleMousedown( event ){}
-
-function debugUpdate(){
-	debug1.innerHTML = "";
-	debug2.innerHTML = "";
-	debug3.innerHTML = "";
-	debug4.innerHTML = "";
+function move(particle){ //update particle location
+  particle.x += particle.dx
+  particle.y += particle.dy
+  particle.dy+= GRAVITY
+  particle.dx+= WIND
+  particle.dx *= AIR_FRICTION
+  if( (particle.y >= HEIGHT || particle.y <= 0) && particle.bounced < bounceLimit ){// floor/ceiling bounce
+    particle.y -= particle.dy
+    particle.dy = -particle.dy * BOUNCE
+    particle.bounced++
+  }
+  if( particle.x >= WIDTH - particleSize || particle.x <= 0 ){ // wall bounce
+    particle.dx = -particle.dx
+  }
 }
